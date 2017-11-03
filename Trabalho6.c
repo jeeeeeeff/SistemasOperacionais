93a6e996af96b55e2701513425ee64f2786595b5
jeeeeeeff <31854869+jeeeeeeff@users.noreply.github.com>
2017-11-03 14:47:59 -0200

    Update Trabalho6.c

--- Trabalho6.c
+++ Trabalho6.c
@@ -2,29 +2,29 @@
 
 int main(int argc, char *argv[])
 {
-    char id[10];
+    char id[10], nro[10];
     int smid = shmget(123456789, 1024, IPC_CREAT|SHM_R|SHM_W);
     //somente pra iniciar
-    sems *las = shmat(smid, 0, 0);
-    init(las);
+    //sems *las = shmat(smid, 0, 0);
+    //init(las);
     sprintf(id, "%d", smid);
-    printf("%d\n", atoi(argv[1]));
     int i, x = atoi(argv[1]);
 
-    for(i = 0; i < x; i++)
+    for(i = 0; i < atoi(argv[1]); i++)
+    {
+        
+        sprintf(nro, "%d", i);
         if(fork() > 0)
-        {
-            execl("./prod", id, NULL);
-            puts("Fazendo os caras cons");
-            sleep(10);
-        }
+            execl("./prod", "", id, nro, NULL);
+    }
 
-    /*for(i = 0; i < atoi(argv[2]) - 1; i++)
+    for(i = 0; i < atoi(argv[2]) - 1; i++)
+    {
+        sprintf(nro, "%d", i);
         if(fork() > 0)
-        {
-            execl("./cons", id, NULL);
-            puts("Fazendo os caras prod");
-        }
-
-    execl("./cons", id, NULL);*/
+            execl("./cons", "", id, nro, NULL);
+    }
+        
+    sprintf(nro, "%d", i);
+    execl("./cons", "", id, nro, NULL);
 }
